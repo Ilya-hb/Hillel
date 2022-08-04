0b559cda090522468a1683767843c6350780f818
Ilya <ilyholu@gmail.com>
2022-08-04 19:51:41 +0300

    quick fix

--- 24.07.2022H/main.js
+++ 24.07.2022H/main.js
@@ -2,37 +2,37 @@ const books = [
     {
         title: 'Don Quixote',
         author: 'Miguel de Cervantes',
-        path: 'images/don_quixote.jpeg'
+        path: 'images/don_quixote'
     },
     {
         title: 'Lord of the Rings',
         author: 'J.R.R. Tolkien',
-        path: 'images/lord_of_the_rings.jpeg'
+        path: 'images/lord_of_the_rings'
     },
     {
         title: 'Harry Potter and the Sorcerer\'s Stone',
         author: 'J.K. Rowling',
-        path: 'images/harry_potter.jpeg'
+        path: 'images/harry_potter'
     },
     {
         title: 'Alice\'s Adventures in Wonderland',
         author: 'Lewis Carroll',
-        path: 'images/alice_adventures_in_wonderland.jpeg'
+        path: 'images/alice_adventures_in_wonderland'
     },
     {
         title: 'Twenty Thousand Leagues Under the Sea',
         author: 'Jules Verne',
-        path: 'images/twenty_thousand_leagues_under_the_sea.jpeg'
+        path: 'images/twenty_thousand_leagues_under_the_sea'
     },
     {
         title: 'Moby Dick',
         author: 'Herman Melville',
-        path: 'images/moby_dick.jpeg'
+        path: 'images/moby_dick'
     },
     {
         title: 'The Picture of Dorian Gray',
         author: 'Oscar Wilde',
-        path: 'images/the_picture_of_dorian_gray.jpeg'
+        path: 'images/the_picture_of_dorian_gray'
     }
 ]
 document.write(`<table>
@@ -45,7 +45,7 @@ document.write(`<table>
 for (let i = 0; i < books.length; i++) {
     document.write(`
     <tr>
-        <td><img src = '${books[i].path}'></td>
+        <td><img src = '${books[i].path + '.jpeg'}'></td>
         <td>${books[i].title}</td>
         <td>${books[i].author}</td>
     </tr>
